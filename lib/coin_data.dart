e00f3d59379eba952cac0143fefde1137b7b4bd2
GauravXd <grvdhingra1999@gmail.com>
2020-04-05 09:02:41 +0530

    API up

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -31,14 +31,15 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate/';
+const apiKey = '9E6BBD28-93EC-4D39-9781-FB64E8A5050E';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
     //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
     //TODO 5: Return a Map of the results instead of a single value.
-    String requestURL = '$coinAPIURL/BTC$selectedCurrency';
+    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
+    // https://rest.coinapi.io/v1/exchangerate/BTC/INR?apikey=9E6BBD28-93EC-4D39-9781-FB64E8A5050E
     http.Response response = await http.get(requestURL);
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
